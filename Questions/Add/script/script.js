11ed7b50963643f1fdadef4f3321e548da0d704d
Moisés Córdova <kernel_linux@outlook.com>
2018-12-05 21:27:02 -0600

    ID of button changed

--- Questions/Add/script/script.js
+++ Questions/Add/script/script.js
@@ -5,9 +5,9 @@ var header = new Headers();
 header.append("Content-Type", "application/json");
 header.append("Authorization", "Bearer " + cookie);
 
-var button = document.getElementById('logout');
+var logoutButton = document.getElementById('logout');
 
-button.addEventListener('click', function() {
+logoutButton.addEventListener('click', function() {
   setCookie('session', '', 'Thu, 01 Jan 1970 00:00:00 UTC');
   location.href = '../../Main-Page/index.html';
 })
